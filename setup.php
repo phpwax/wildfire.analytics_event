aa0af3af93233c453235cb8804f86efdad73511f
Charles Marshall <charles@oneblackbear.com>
2013-07-03 17:17:17 +0100

    adding extra things to the content model

--- setup.php
+++ setup.php
@@ -1,12 +1,15 @@
 <?
-
 AutoLoader::register_view_path("plugin", __DIR__."/view/");
 AutoLoader::$plugin_array[] = array("name"=>"wildfire.analytics_event","dir"=>__DIR__);
 
+echo CONTENT_MODEL;exit;
 if(defined("CONTENT_MODEL")){
+
   WaxEvent::add(CONTENT_MODEL.".setup", function(){
     $model = WaxEvent::data();
-    $model->define("track_page_in_analytics", "BooleanField");
+    $model->define("track_as_event_in_analytics", "BooleanField", array('group'=>'Analytics Event') );
+    $model->define("analytics_event_action", "CharField", array('group'=>'Analytics Event') );
+    $model->define("analytics_event_label", "CharField", array('group'=>'Analytics Event') );
   });
 }
 ?>
